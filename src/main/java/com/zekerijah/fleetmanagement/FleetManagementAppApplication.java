3bac6c38ddffd8ba92fb7a920318d245d8fc28b4
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-29 14:47:23 +0200

    First commit

---
+++ src/main/java/com/zekerijah/fleetmanagement/FleetManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.zekerijah.fleetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zekerijah/fleetmanagement/FleetManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekerijah.fleetmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
