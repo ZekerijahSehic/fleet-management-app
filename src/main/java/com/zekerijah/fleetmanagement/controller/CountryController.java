4005b48f30ff7f1cbf6a1be50dfde5d9eff5a713
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-08 15:56:25 +0200

    Display country

--- src/main/java/com/zekerijah/fleetmanagement/controller/CountryController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/CountryController.java
@@ -1,13 +1,25 @@
 package com.zekerijah.fleetmanagement.controller;
 
+
+import com.zekerijah.fleetmanagement.model.Country;
+import com.zekerijah.fleetmanagement.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String getCountries(Model model){
+        List<Country> countriyList = countryService.getListCountry();
+        model.addAttribute("countries", countriyList);
         return "country";
     }
 }
