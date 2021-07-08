c5a3b70f840d1320e05caf45ad6c83983557b8c3
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-08 15:55:13 +0200

    Method for all countries

--- src/main/java/com/zekerijah/fleetmanagement/service/CountryService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/CountryService.java
@@ -1,7 +1,19 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Country;
+import com.zekerijah.fleetmanagement.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getListCountry() {
+        return countryRepository.findAll();
+    }
 }
