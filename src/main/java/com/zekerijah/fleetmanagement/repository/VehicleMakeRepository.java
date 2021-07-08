b1aaff008a287a97b2f85bdf0cc3e168d3f0003a
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-08 11:30:47 +0200

    Fix T in JpaRep...

--- src/main/java/com/zekerijah/fleetmanagement/repository/VehicleMakeRepository.java
+++ src/main/java/com/zekerijah/fleetmanagement/repository/VehicleMakeRepository.java
@@ -1,8 +1,9 @@
 package com.zekerijah.fleetmanagement.repository;
 
+import com.zekerijah.fleetmanagement.model.VehicleMake;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleMakeRepository extends JpaRepository<VehicleMakeRepository, Integer> {
+public interface VehicleMakeRepository extends JpaRepository<VehicleMake, Integer> {
 }
