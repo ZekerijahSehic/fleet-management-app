2a6c0733d753620f93c11ea01152bb63f5e93a1c
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-12 13:00:59 +0200

    Get country by id FIX

--- src/main/java/com/zekerijah/fleetmanagement/controller/CountryController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/CountryController.java
@@ -6,10 +6,7 @@ import com.zekerijah.fleetmanagement.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,10 +30,9 @@ public class CountryController {
         return "redirect:/countries";
     }
 
-    @RequestMapping("/countries/findById/")
+    @RequestMapping("/countries/findById")
     @ResponseBody
     public Optional<Country> findById(int id) {
         return countryService.findById(id);
     }
-
 }
