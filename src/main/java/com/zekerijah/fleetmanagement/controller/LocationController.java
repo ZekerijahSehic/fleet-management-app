2c6edf88a525b8bf97ff7269b7a623216e331163
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-12 16:53:02 +0200

    Location controller&service

--- src/main/java/com/zekerijah/fleetmanagement/controller/LocationController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/LocationController.java
@@ -1,13 +1,55 @@
 package com.zekerijah.fleetmanagement.controller;
 
+import com.zekerijah.fleetmanagement.model.Location;
+import com.zekerijah.fleetmanagement.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LocationController {
 
+    @Autowired
+    private LocationService locationService;
+
     @GetMapping("/locations")
-    public String getLoacations(){
+    public String getLoacations(Model model){
+        List<Location> locations = locationService.getAllLocations();
+        model.addAttribute("locations", locations);
         return "location";
     }
+
+    @PostMapping("/locations/addnew")
+    public String addNew(Location location){
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping("/locations/findById")
+    @ResponseBody
+    public Optional<Location> findLocationById(Integer id){
+        return locationService.findById(id);
+    }
+
+    @RequestMapping(value = "/locations/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String updateLocation(Location location){
+        locationService.save(location);
+        return "redirect:/locations";
+    }
+
+    @RequestMapping(value = "/locations/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String deleteLocation(Integer id){
+        locationService.delete(id);
+        return "redirect:/locations";
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/zekerijah/fleetmanagement/service/LocationService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/LocationService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Location;
+import com.zekerijah.fleetmanagement.repository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public List<Location> getAllLocations(){
+        return locationRepository.findAll();
+    }
+
+    public Location save(Location location){
+        return locationRepository.save(location);
+    }
+
+    public Optional<Location> findById(Integer id){
+        return locationRepository.findById(id);
+    }
+
+    public void delete(Integer id){
+        locationRepository.deleteById(id);
+    }
 }
