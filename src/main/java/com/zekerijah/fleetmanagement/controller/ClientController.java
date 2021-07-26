404319daba1e5f2daa6f02214b816ac52af6aae3
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-26 17:04:52 +0200

    Client controller&service

--- src/main/java/com/zekerijah/fleetmanagement/controller/ClientController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/ClientController.java
@@ -1,13 +1,66 @@
 package com.zekerijah.fleetmanagement.controller;
 
+import com.zekerijah.fleetmanagement.model.Client;
+import com.zekerijah.fleetmanagement.model.Country;
+import com.zekerijah.fleetmanagement.model.State;
+import com.zekerijah.fleetmanagement.service.ClientService;
+import com.zekerijah.fleetmanagement.service.CountryService;
+import com.zekerijah.fleetmanagement.service.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ClientController {
 
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private CountryService countryService;
+
+    @Autowired
+    private StateService stateService;
+
     @GetMapping("/clients")
-    public String getClients(){
+    public String getClients(Model model){
+        List<Client> clients = clientService.getAllClients();
+        model.addAttribute("clients", clients);
+
+        List<Country> countries = countryService.getListCountry();
+        model.addAttribute("countries", countries);
+
+        List<State> states = stateService.getListState();
+        model.addAttribute("states", states);
+
         return "client";
+    }
+
+    @PostMapping("/clients/addnew")
+    public String saveClient(Client client){
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping("/clients/findById")
+    @ResponseBody
+    public Optional<Client> findById(int id){
+        return clientService.getById(id);
+    }
+
+    @RequestMapping(value = "/clients/update", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(Client client){
+        clientService.save(client);
+        return "redirect:/clients";
+    }
+
+    @RequestMapping(value = "/clients/delete", method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(int id){
+        clientService.delete(id);
+        return "redirect:/clients";
     }
 }
--- src/main/java/com/zekerijah/fleetmanagement/service/ClientService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/ClientService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Client;
+import com.zekerijah.fleetmanagement.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public List<Client> getAllClients(){
+        return clientRepository.findAll();
+    }
+
+    public Client save(Client client){
+        return clientRepository.save(client);
+    }
+
+    public Optional<Client> getById(int id){
+        return clientRepository.findById(id);
+    }
+
+    public void delete(int id){
+        clientRepository.deleteById(id);
+    }
 }
