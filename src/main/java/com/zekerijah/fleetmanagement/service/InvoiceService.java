c89179dad85fb1347d28fd37402ebced1d8d6c53
Zekerijah <zekerijahsehic1@gmail.com>
2021-07-27 17:07:28 +0200

    Invoice service

--- src/main/java/com/zekerijah/fleetmanagement/service/InvoiceService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/InvoiceService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Invoice;
+import com.zekerijah.fleetmanagement.repository.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public List<Invoice> getAllInvoices(){
+        return invoiceRepository.findAll();
+    }
+
+    public void save(Invoice invoice){
+        invoiceRepository.save(invoice);
+    }
+
+    public Optional<Invoice> getById(Integer id){
+        return invoiceRepository.findById(id);
+    }
+
+    public void delete(Integer id) {
+        invoiceRepository.deleteById(id);
+    }
 }
