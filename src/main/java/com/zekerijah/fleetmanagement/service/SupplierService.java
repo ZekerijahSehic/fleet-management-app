5c5bd980ae827698b981c90c832275710517e98e
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-02 11:36:27 +0200

    Supplire service

--- src/main/java/com/zekerijah/fleetmanagement/service/SupplierService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/SupplierService.java
@@ -1,7 +1,33 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Supplier;
+import com.zekerijah.fleetmanagement.repository.SupplierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupplierService {
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    public List<Supplier> getAllSuppliers(){
+        return supplierRepository.findAll();
+    }
+
+    public void save(Supplier supplier){
+        supplierRepository.save(supplier);
+    }
+
+    public Optional<Supplier> getById(Integer id){
+        return supplierRepository.findById(id);
+    }
+
+    public void delete(Integer id){
+        supplierRepository.deleteById(id);
+    }
+
 }
