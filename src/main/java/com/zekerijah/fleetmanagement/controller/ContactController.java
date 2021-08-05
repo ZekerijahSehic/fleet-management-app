d4e2c19babbc8fca4d2568fab1e683271b827c0a
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-05 15:56:26 +0200

    Contact controller

--- src/main/java/com/zekerijah/fleetmanagement/controller/ContactController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/ContactController.java
@@ -1,13 +1,49 @@
 package com.zekerijah.fleetmanagement.controller;
 
+import com.zekerijah.fleetmanagement.model.Contact;
+import com.zekerijah.fleetmanagement.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ContactController {
 
+    @Autowired
+    private ContactService contactService;
+
     @GetMapping("/contacts")
-    public String getContacts(){
+    public String getContacts(Model model) {
+        List<Contact> contactList = contactService.getAllContacts();
+        model.addAttribute("contacts", contactList);
         return "contact";
+    }
+
+    @PostMapping("/contacts/addnew")
+    public String saveContact(Contact contact){
+        contactService.save(contact);
+        return "redirect:/contacts";
+    }
+
+    @RequestMapping("/contacts/findById")
+    @ResponseBody
+    public Optional<Contact> findById(int id){
+        return contactService.getById(id);
+    }
+
+    @RequestMapping(value = "/contacts/update", method = {RequestMethod.GET, RequestMethod.PUT})
+    public String update(Contact contact){
+        contactService.save(contact);
+        return "redirect:/contacts";
+    }
+
+    @RequestMapping(value = "/contacts/delete", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(int id){
+        contactService.delete(id);
+        return "redirect:/contacts";
     }
 }
