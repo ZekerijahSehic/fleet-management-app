d6bffeca8164bff41ee8db8efd86f20e319a37f6
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-06 15:15:23 +0200

    Get all vehicle

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleService.java
@@ -1,7 +1,19 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.Vehicle;
+import com.zekerijah.fleetmanagement.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    public List<Vehicle> getAllVehicles(){
+        return vehicleRepository.findAll();
+    }
 }
