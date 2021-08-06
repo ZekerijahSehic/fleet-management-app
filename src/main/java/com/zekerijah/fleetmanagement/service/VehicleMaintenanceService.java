7d41199aac0b4ce89caf01f32f84d349b31f4702
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-06 15:03:47 +0200

    VehicleMaintenance Service

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleMaintenanceService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleMaintenanceService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.VehicleMaintenance;
+import com.zekerijah.fleetmanagement.repository.VehicleMaintenanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMaintenanceService {
+
+    @Autowired
+    private VehicleMaintenanceRepository vehicleMaintenanceRepository;
+
+    public List<VehicleMaintenance> getAllVehicleMaintenances(){
+        return vehicleMaintenanceRepository.findAll();
+    }
+
+    public void save(VehicleMaintenance vehicleMaintenance){
+        vehicleMaintenanceRepository.save(vehicleMaintenance);
+    }
+
+    public Optional<VehicleMaintenance> getById(int id){
+        return vehicleMaintenanceRepository.findById(id);
+    }
+
+    public void delete(int id){
+        vehicleMaintenanceRepository.deleteById(id);
+    }
 }
