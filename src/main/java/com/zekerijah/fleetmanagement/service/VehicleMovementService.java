f828d8b5bddb9d4b2e406e03d34be3e44c431934
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-12 15:35:51 +0200

    Vehicle Movement Service

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleMovementService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleMovementService.java
@@ -1,7 +1,32 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.VehicleMovement;
+import com.zekerijah.fleetmanagement.repository.VehicleMovementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMovementService {
+
+    @Autowired
+    private VehicleMovementRepository vehicleMovementRepository;
+
+    public List<VehicleMovement> getAllVehicleMovements(){
+        return vehicleMovementRepository.findAll();
+    }
+
+    public void save(VehicleMovement vehicleMovement){
+        vehicleMovementRepository.save(vehicleMovement);
+    }
+
+    public Optional<VehicleMovement> getVehicleMovementById(int id){
+        return vehicleMovementRepository.findById(id);
+    }
+
+    public void delete(int id){
+        vehicleMovementRepository.deleteById(id);
+    }
 }
