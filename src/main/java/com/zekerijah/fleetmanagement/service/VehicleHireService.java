48cfcb489824d2afb07ec9a048497247701351c3
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-18 17:03:25 +0200

    VehicleHire get, save

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
@@ -1,12 +1,25 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.model.VehicleHire;
 import com.zekerijah.fleetmanagement.repository.VehicleHireRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleHireService {
 
     @Autowired
     private VehicleHireRepository vehicleHireRepository;
+
+    public List<VehicleHire> getAllVehicleHires(){
+        return vehicleHireRepository.findAll();
+    }
+
+    public VehicleHire save(VehicleHire vehicleHire){
+        return vehicleHireRepository.save(vehicleHire);
+    }
+
+
 }
