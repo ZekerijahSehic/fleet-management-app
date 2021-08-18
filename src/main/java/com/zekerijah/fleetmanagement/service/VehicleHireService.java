4ea35dce71cfbe88b99433ee74eac59fe5d29a23
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-18 17:00:07 +0200

    Vehicle hire service

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
@@ -1,7 +1,12 @@
 package com.zekerijah.fleetmanagement.service;
 
+import com.zekerijah.fleetmanagement.repository.VehicleHireRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleHireService {
+
+    @Autowired
+    private VehicleHireRepository vehicleHireRepository;
 }
