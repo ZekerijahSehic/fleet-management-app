3a1819c0e04944f1069053855965618c42f682eb
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-18 17:05:45 +0200

    VehicleHire serv completed

--- src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
+++ src/main/java/com/zekerijah/fleetmanagement/service/VehicleHireService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleHireService {
@@ -19,6 +20,14 @@ public class VehicleHireService {
 
     public VehicleHire save(VehicleHire vehicleHire){
         return vehicleHireRepository.save(vehicleHire);
+    }
+
+    public Optional<VehicleHire> getVehicleHireById(int id){
+        return vehicleHireRepository.findById(id);
+    }
+
+    public void delete(int id){
+        vehicleHireRepository.deleteById(id);
     }
 
 
