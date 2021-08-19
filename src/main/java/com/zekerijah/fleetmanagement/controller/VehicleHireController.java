f7ed40184f253881c9cc6210e886330cc70afc4f
Zekerijah <zekerijahsehic1@gmail.com>
2021-08-19 21:05:49 +0200

    Vehicle Hire controller

--- src/main/java/com/zekerijah/fleetmanagement/controller/VehicleHireController.java
+++ src/main/java/com/zekerijah/fleetmanagement/controller/VehicleHireController.java
@@ -1,10 +1,15 @@
 package com.zekerijah.fleetmanagement.controller;
 
+import com.zekerijah.fleetmanagement.service.VehicleHireService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleHireController {
+
+    @Autowired
+    private VehicleHireService vehicleHireService;
 
     @GetMapping("/vehicle-hires")
     public String getVehicleHires(){
